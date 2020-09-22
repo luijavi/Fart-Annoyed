8ee9f6e480e68c7979493a90b60c24256334ff95
luijavi <luijavi@gmail.com>
2020-09-22 18:06:35 -0500

    created RectF class

---
+++ Engine/RectF.cpp
@@ -0,0 +1,28 @@
+#include "RectF.h"
+
+RectF::RectF(float left_in, float right_in, float top_in, float bottom_in)
+	:
+	left(left_in),
+	right(right_in),
+	top(top_in),
+	bottom(bottom_in)
+{
+}
+
+RectF::RectF(const Vec2& topLeft, const Vec2& bottomRight)
+	:
+	RectF(topLeft.x, bottomRight.x, topLeft.y, bottomRight.y)
+{
+}
+
+RectF::RectF(const Vec2& topLeft, float width, float height)
+	:
+	RectF(topLeft, topLeft + Vec2(width, height))
+{
+}
+
+bool RectF::IsOverlappingWith(const RectF& otherRect) const
+{
+	return right > otherRect.left && left < otherRect.right &&
+		top < otherRect.bottom && bottom > otherRect.top;
+}
