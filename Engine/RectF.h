3e50df0e82ce6f761ee535b15703920bacab869a
luijavi <luijavi@gmail.com>
2020-09-22 12:30:01 -0500

    created RectF

---
+++ Engine/RectF.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Vec2.h"
+
+class RectF
+{
+public:
+	RectF() = default;
+	RectF(float left_in, float right_in, float top_in, float bottom_in);
+	RectF(const Vec2& topLeft, const Vec2& bottomRight);
+	RectF(const Vec2& topLeft, float width, float height);
+	bool IsOverlappingWith(const RectF& otherRect) const;
+private:
+	float top;
+	float left;
+	float bottom;
+	float right;
+};
